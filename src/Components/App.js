ebe47dd05dabf1c7fdec25e63af5637b6ee3818a
Kim G <xiaole.guo@yahoo.com>
2022-06-29 19:27:06 -0600

    update App.js to remove underline

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,35 +1,26 @@
-import { SiGithub, SiLinkedin } from "react-icons/si";
+import React from 'react'
+import {Routes, Route, Link} from 'react-router-dom' 
 import '../SassStyles/App.scss';
+import Landing from './Landing'
+import Projects from './Projects';
 import resume from '../Assets/resume.pdf'
-import profile from '../Assets/profile.jpeg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Xiaole Guo</h1>
+        <Link style={{textDecoration: 'none'}} to="/"><h1>Xiaole Guo</h1></Link>
         <nav>
           <a href={resume}>Resume</a>
-          <a href={resume}>Tech Projects</a>
-          <a href={resume}>Other Work</a>
+          <Link to="/tech-projects">Tech Projects</Link>
         </nav>
       </header>
       <main>
-        <div className="title-pic-container">
-          <img src={profile} alt="profile picture of website author" />
-          <div className="link-container">
-            <a href="https://www.linkedin.com/in/xiaole-guo-5331b4158/"><SiLinkedin className="icon"/></a>
-            <a href="https://github.com/kg-byte"><SiGithub className="icon"/></a>
-          </div>
-        </div>
-        <div className="info-container">
-          <h2>Hello and welcome!</h2>
-          <p>I am an avid beginner golfer, an anime lover, and a proud Chiweenie owner.</p>
-          <p>Coming from a dynamic industry like chemical engineering, I thrive in energetic and fast-paced environments.</p>
-          <p>Problem-solving is my passion and what motivated me to become a software developer. I believe in fairness, honesty, and taking the initiative to get work done.</p>
-          <p>As a software engineer, I strive to write clean, and organized code that is well-tested, to maintain a focused work flow, and to foster meaningful work relationships.</p>
-          <p>If you'd like to be in touch, then please email me! guoxiaole@mines.edu </p>
-        </div>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/tech-projects" element={<Projects />} />
+          <Route path="/*" element={<p className="error">404: What you requested does not exist 🤷🏼‍♀️</p>}/>
+        </Routes>
       </main>
     </div>
   );
