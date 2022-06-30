c9fe22ede95c83d921c110175153310050be9eda
Kim G <xiaole.guo@yahoo.com>
2022-06-29 19:26:49 -0600

    update to Hashrouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom'
+import ReactDOM from 'react-dom';
+import {HashRouter} from 'react-router-dom'
 import './SassStyles/index.scss';
 import App from './Components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
+ReactDOM.render(
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>,
+  document.getElementById('root')
 );
